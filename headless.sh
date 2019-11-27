d94ffb33c9d524cb54215debffe9d89950d62732
Max Nasonov <maxim@nasonov.me>
2019-11-27 12:41:53 +0700

    Make more pieces customizable

--- headless.sh
+++ headless.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 OSK="ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc"
-VMDIR=$PWD
-OVMF=$VMDIR/firmware
 
 [[ -z "$MEM" ]] && {
 	MEM="1G"
@@ -12,27 +10,42 @@ OVMF=$VMDIR/firmware
 	CPUS=1
 }
 
-[[ -z "$SYSTEM_DISK" ]] && {
+
+[[ -z "$INSTALLATION_DISK" ]] || {
+    INSTALLATION_DISK=BaseSystem.img
+}
+
+[[ -z "$VM_NAME" ]] && {
+    VM_NAME=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
+}
+
+if [[ -z "$VMDIR" ]]; then
+    VMDIR=$PWD
+else
+    cp $PWD/firmware/OVMF_CODE.fd $VMDIR/${VM_NAME}_OVMF_CODE.fd
+    cp $PWD/firmware/OVMF_VARS-1024x768.fd $VMDIR/${VM_NAME}_OVMF_VARS-1024x768.fd
+    cp $PWD/ESP.qcow2 $VMDIR/${VM_NAME}_ESP.qcow2
+fi
+
+[[ -z "$VMDIR/${VM_NAME}.qcow2" ]] && {
     echo "Please set the SYSTEM_DISK environment variable"
     exit 1
 }
 
-[[ -r "$SYSTEM_DISK" ]] || {
-    echo "Can't read system disk image: $SYSTEM_DISK"
+[[ -r "$VMDIR/${VM_NAME}.qcow2" ]] || {
+    echo "Can't read system disk image: $VMDIR/${VM_NAME}.qcow2"
     exit 1
 }
 
-[[ -z "$INSTALLATION_DISK" ]] && {
-    INSTALLATION_DISK=BaseSystem.img
-}
 
 MOREARGS=()
 
 [[ "$HEADLESS" = "1" ]] && {
-    MOREARGS+=(-nographic -vnc :0 -k en-us)
+    MOREARGS+=(-nographic -vnc 127.0.0.1:0 -k en-us)
 }
 
 qemu-system-x86_64 \
+    -name ${VM_NAME} \
     -enable-kvm \
     -m $MEM \
     -machine q35,accel=kvm \
@@ -40,8 +53,8 @@ qemu-system-x86_64 \
     -cpu Penryn,vendor=GenuineIntel,kvm=on,+sse3,+sse4.2,+aes,+xsave,+avx,+xsaveopt,+xsavec,+xgetbv1,+avx2,+bmi2,+smep,+bmi1,+fma,+movbe,+invtsc \
     -device isa-applesmc,osk="$OSK" \
     -smbios type=2 \
-    -drive if=pflash,format=raw,readonly,file="$OVMF/OVMF_CODE.fd" \
-    -drive if=pflash,format=raw,file="$OVMF/OVMF_VARS-1024x768.fd" \
+    -drive if=pflash,format=raw,readonly,file="$VMDIR/${VM_NAME}_OVMF_CODE.fd" \
+    -drive if=pflash,format=raw,file="$VMDIR/${VM_NAME}_OVMF_VARS-1024x768.fd" \
     -vga qxl \
     -usb -device usb-kbd -device usb-tablet \
     -netdev user,id=net0 \
@@ -49,8 +62,8 @@ qemu-system-x86_64 \
     -device ich9-ahci,id=sata \
     -drive id=ESP,if=none,format=qcow2,file=ESP.qcow2 \
     -device ide-hd,bus=sata.2,drive=ESP \
-    -drive id=InstallMedia,format=raw,if=none,file=${INSTALLATION_DISK} \
+    -drive id=InstallMedia,format=raw,if=none,file="${INSTALLATION_DISK}" \
     -device ide-hd,bus=sata.3,drive=InstallMedia \
-    -drive id=SystemDisk,if=none,file="${SYSTEM_DISK}" \
+    -drive id=SystemDisk,if=none,file="$VMDIR/${VM_NAME}.qcow2" \
     -device ide-hd,bus=sata.4,drive=SystemDisk \
     "${MOREARGS[@]}"
--- make.sh
+++ make.sh
@@ -3,7 +3,17 @@
 # make.sh: Generate customized libvirt XML.
 # by Foxlet <foxlet@furcode.co>
 
-VMDIR=$PWD
+[[ -z "$VM_NAME" ]] && {
+    echo "Error: please provide VM_NAME"
+    exit 1
+}
+
+if [[ -z "$VMDIR" ]]; then
+    VMDIR=$PWD
+fi
+
+MAC_ADDRESS=$(python /scripts/random_mac_for_macos.py)
+UUID=$(uuidgen)
 MACHINE="$(qemu-system-x86_64 --machine help | grep q35 | cut -d" " -f1 | grep -Eoe ".*-[0-9.]+" | sort -rV | head -1)"
 OUT="template.xml"
 
@@ -21,7 +31,7 @@ error() {
 }
 
 generate(){
-    sed -e "s|VMDIR|$VMDIR|g" -e "s|MACHINE|$MACHINE|g" tools/template.xml.in > $OUT
+    sed -e "s|VMDIR|$VMDIR|g" -e "s|VM_NAME|$VM_NAME|g" -e "s|MAC_ADDRESS|$MAC_ADDRESS|g" -e "s|UUID|$UUID|g"  -e "s|MACHINE|$MACHINE|g" tools/template.xml.in > $OUT
     echo "$OUT has been generated in $VMDIR"
 }
 
